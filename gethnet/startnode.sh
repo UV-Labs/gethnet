b9261f0075f0224a2f7aef39d381e83f7d5b4b65
paritomarrr <pari2019cse083abesit@gmail.com>
2022-03-09 09:25:14 +0530

    new

---
+++ gethnet/startnode.sh
@@ -0,0 +1,1 @@
+geth --networkid 4223 --mine --miner.threads=2 --datadir "." --nodiscover --rpc --rpcport "8545" --port "30303" --rpccorsdomain "*" --nat "any" --rpcapi eth,web3,personal,net --unlock 0 --password ./password.sec --ipcpath "~/.ethereum/geth.ipc"
